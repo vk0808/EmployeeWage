d6066f01fd90958a0664337d12996b153db48a45
Vinod Kumar R <vk4765712@gmail.com>
2021-07-16 12:41:14 +0530

    added: employee wage cal part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,19 +3,23 @@
 echo "Welcome to the Employee Wage Computation"
 
 
-# Variable
-IS_PRESENT=1
+# Variables
+IS_HALF_TIME=1
+IS_FULL_TIME=2
 WAGE_PER_HR=20
-empPresent=$(( RANDOM % 2))
+
+empPresent=$(( RANDOM % 3 ))
 
 
 # Check if Employee is present or not
-if [ $empPresent -eq $IS_PRESENT ] ;
-then
+if [ $empPresent -eq $IS_FULL_TIME ] ; then
         empHrs=8
+elif [ $empPresent -eq $IS_HALF_TIME ]; then
+        empHrs=4
 else
         empHrs=0
 fi
+
 
 # Calculation
 salary=$(( empHrs * WAGE_PER_HR ))
