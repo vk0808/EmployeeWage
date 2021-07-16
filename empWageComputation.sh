5bfab936c0267ec8779a9ee30e61b69d573f5232
Vinod Kumar R <vk4765712@gmail.com>
2021-07-16 13:01:04 +0530

    added: employee wage cal for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,12 +11,35 @@ WAGE_PER_HR=20
 empPresent=$(( RANDOM % 3 ))
 
 
-# Check if Employee is present or not
-case $empPresent in
-        $IS_HALF_TIME) empHrs=4 ;;
-        $IS_FULL_TIME) empHrs=8 ;;
-        *) empHrs=0 ;;
-esac
+
+for ((day=1;day<=$MAX_WORK_DAYS;day++))
+do
+
+	# Check if Employee is present or not
+        empPresent=$(( RANDOM % 3))
+
+
+	# Determine work hour of the employee
+        case $empPresent in
+                $IS_HALF_TIME) empHrs=4 ;;
+                $IS_FULL_TIME) empHrs=8 ;;
+                *) empHrs=0 ;;
+        esac
+
+
+        # Calculation total work hour
+        totalWorkHr=$(( totalWorkHr + empHrs ))
+
+
+        # Calculation total salary
+	basePay=$(( empHrs * WAGE_PER_HR ))
+        salary=$(( basePay + salary ))
+done
+
+
+
+
+
 
 # Calculation
 salary=$(( empHrs * WAGE_PER_HR ))
