0701a9a68e8600939ce67130f3eed3c67121cedf
Vinod Kumar R <vk4765712@gmail.com>
2021-07-16 12:26:57 +0530

    added: wage cal feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,13 +5,17 @@ echo "Welcome to the Employee Wage Computation"
 
 # Variable
 IS_PRESENT=1
+WAGE_PER_HR=20
 empPresent=$(( RANDOM % 2))
 
 
 # Check if Employee is present or not
 if [ $empPresent -eq $IS_PRESENT ] ;
 then
-        echo "Employee is present"
+        empHrs=8
 else
-        echo "Employee is absent"
+        empHrs=0
 fi
+
+# Calculation
+salary=$(( empHrs * WAGE_PER_HR ))
