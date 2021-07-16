6eb10df5d674cdc86f43a22fe9f88e1b4c77577d
Vinod Kumar R <vk4765712@gmail.com>
2021-07-16 12:06:26 +0530

    added: employee check feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
 #! /bin/bash -x
 
-echo "Welcome to the Employee Computation"
+echo "Welcome to the Employee Wage Computation"
+
+
+# Variable
+IS_PRESENT=1
+empPresent=$(( RANDOM % 2))
+
+
+# Check if Employee is present or not
+if [ $empPresent -eq $IS_PRESENT ] ;
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
